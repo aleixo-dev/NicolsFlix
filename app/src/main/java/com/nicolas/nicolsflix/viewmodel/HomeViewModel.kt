e1fca7c65fdc6904986b440efe3501aa89fae908
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-08-12 17:54:17 -0300

    view model refatorado e implementado novas chamadas

--- app/src/main/java/com/nicolas/nicolsflix/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/nicolas/nicolsflix/viewmodel/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.nicolas.nicolsflix.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,26 +8,43 @@ import com.nicolas.nicolsflix.data.model.Movie
 import com.nicolas.nicolsflix.repository.api.MovieApiRepositoryImpl
 import kotlinx.coroutines.launch
 
-class HomeViewModel(private val movieApiRepositoryImpl: MovieApiRepositoryImpl) : ViewModel() {
+class HomeViewModel(
+    private val movieApiRepositoryImpl: MovieApiRepositoryImpl
+) : ViewModel() {
 
-    val listMovieTrending = MutableLiveData<ArrayList<Movie>>()
-    val listSearchMovie = MutableLiveData<ArrayList<Movie>>()
+    private val _listTrendingMovie = MutableLiveData<ArrayList<Movie>>()
+    val listTrendingMovie: LiveData<ArrayList<Movie>> = _listTrendingMovie
+
+    private val _listNamesMovie = MutableLiveData<ArrayList<Movie>>()
+    val listNamesMovie: LiveData<ArrayList<Movie>> = _listNamesMovie
+
+    private val _listPopularMovie = MutableLiveData<ArrayList<Movie>>()
+    val listPopularMovie: LiveData<ArrayList<Movie>> = _listPopularMovie
 
     init {
-        callMovieTrending()
+        fetchTrendingMovies()
     }
 
-
-    private fun callMovieTrending() {
+    private fun fetchTrendingMovies() {
         viewModelScope.launch {
-            listMovieTrending.value = movieApiRepositoryImpl.getMovieTrending()
+            _listTrendingMovie.value = movieApiRepositoryImpl.getMovieTrending()
         }
     }
 
-    fun callSearchMovie(titleMovie: String) {
+    fun fetchPopularMovie() {
         viewModelScope.launch {
-            listSearchMovie.value = movieApiRepositoryImpl.getMovieSearch(titleMovie)
+            _listPopularMovie.value = movieApiRepositoryImpl.getPopularMovie()
         }
     }
 
+    fun fetchNameMovie(titleMovie: String) {
+        viewModelScope.launch {
+            val result = movieApiRepositoryImpl.getMovieSearch(titleMovie)
+            if (result.isNullOrEmpty()) {
+                _listNamesMovie.value = ArrayList()
+            } else {
+                _listNamesMovie.value = result!!
+            }
+        }
+    }
 }
