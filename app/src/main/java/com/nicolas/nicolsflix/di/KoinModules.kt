14dd44e2f29e9057a9452a38b7754b203518912b
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-08-12 18:02:38 -0300

    melhoria para modulo de injeção de dependência do app

--- app/src/main/java/com/nicolas/nicolsflix/di/KoinModules.kt
+++ app/src/main/java/com/nicolas/nicolsflix/di/KoinModules.kt
@@ -5,22 +5,29 @@ import com.nicolas.nicolsflix.data.db.NicolsDatabase
 import com.nicolas.nicolsflix.data.network.RetrofitInitializer
 import com.nicolas.nicolsflix.repository.api.MovieApiRepositoryImpl
 import com.nicolas.nicolsflix.repository.database.MovieDaoRepositoryImpl
+import com.nicolas.nicolsflix.service.MovieService
 import com.nicolas.nicolsflix.viewmodel.DetailsViewModel
 import com.nicolas.nicolsflix.viewmodel.HomeViewModel
 import com.nicolas.nicolsflix.viewmodel.MyListViewModel
 import com.nicolas.nicolsflix.viewmodel.SearchViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val apiModules = module {
     val retrofit = Retrofit.Builder()
         .baseUrl("https://api.themoviedb.org/3/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(provideOkHttpClient())
         .build()
 
     single { RetrofitInitializer(retrofit = retrofit) }
+    single { provideRetrofitService(get()) }
+    single { provideOkHttpClient() }
 }
 
 val databaseModule = module {
@@ -73,3 +80,19 @@ val searchModules = module {
         )
     }
 }
+
+fun provideOkHttpClient(): OkHttpClient {
+
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+    return OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+}
+
+fun provideRetrofitService(retrofit: Retrofit): MovieService =
+    retrofit.create(MovieService::class.java)
