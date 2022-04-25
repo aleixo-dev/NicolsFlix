b83e39c6a62b8ba186c8a09d5dd062ff44012d0a
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-04-25 14:40:31 -0300

    added new Constants.kt to endpoint movies

--- app/src/main/java/com/nicolas/nicolsflix/common/Constants.kt
+++ app/src/main/java/com/nicolas/nicolsflix/common/Constants.kt
@@ -3,6 +3,8 @@ package com.nicolas.nicolsflix.common
 object Constants {
 
     const val API_KEY = "cead12b729988cec6e29f8bfd5d35116"
+    const val ENDPOINT_MOVIES_VIDEOS = "movie/{movie_id}/videos"
+    const val ENDPOINT_PERSON_CREDITS = "movie/{movie_id}/credits"
     const val ENDPOINT_TRENDING = "trending/all/day"
     const val ENDPOINT_SEARCH = "search/movie"
     const val ENDPOINT_GENRE = "genre/movie/list"
@@ -12,4 +14,6 @@ object Constants {
     const val LANGUAGE_BR = "pt-BR"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val LANGUAGE_US = "en-US"
+
+    const val LOAD_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
 }
