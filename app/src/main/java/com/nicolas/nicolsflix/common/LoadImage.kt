b3841bab85786f77a1ad09e9763aaa827e9001c5
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-06-10 22:53:04 -0300

    :construction: refactor code service api

--- app/src/main/java/com/nicolas/nicolsflix/common/LoadImage.kt
+++ app/src/main/java/com/nicolas/nicolsflix/common/LoadImage.kt
@@ -2,6 +2,7 @@ package com.nicolas.nicolsflix.common
 
 import android.content.Context
 import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 
@@ -13,4 +14,19 @@ object LoadImage {
             .diskCacheStrategy(DiskCacheStrategy.DATA)
             .into(view)
     }
+}
+
+fun ImageView.loadImage(context: Context,url: String) {
+
+    val circularProgressDrawable = CircularProgressDrawable(context)
+    circularProgressDrawable.strokeWidth = 5f
+    circularProgressDrawable.centerRadius = 30f
+    circularProgressDrawable.start()
+
+    Glide.with(context)
+        .load(url)
+        .centerCrop()
+        .placeholder(circularProgressDrawable)
+        .diskCacheStrategy(DiskCacheStrategy.DATA)
+        .into(this)
 }
